02ba7bbb623e1a296cec71f27d234516b38ff564
MunawwarQamar <Munawwarbaba2004@gmail.com>
2024-09-08 08:50:38 +0300

    Add Login and signup pages

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import necessary components from react-router-dom
+import Login from '../components/Login/Login'; // Import Login component
+import Signup from '../components/Signup/Signup'; // Import Signup component
+
+const LinkLoginPage = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Route for Login page */}
+        <Route path="/login" element={<Login />} />
+        
+        {/* Route for Signup page */}
+        <Route path="/signup" element={<Signup />} />
+        
+        {/* Redirect to Login page by default */}
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default LinkLoginPage;
